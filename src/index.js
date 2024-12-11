6bb9f82c20ef20d1a9417e46d1908126a40d9105
PaulaARodriguezG <parulagutierrez@gmail.com>
2024-12-10 20:18:09 -0500

    avance6

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';  // Asegúrate de importar 'react-dom/client'
+import ReactDOM from 'react-dom/client';  
 import App from './App';
 import './assets/css/tailwind.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));  // Crear la raíz de la aplicación
+const root = ReactDOM.createRoot(document.getElementById('root'));  
 
 root.render(
     <React.StrictMode>
